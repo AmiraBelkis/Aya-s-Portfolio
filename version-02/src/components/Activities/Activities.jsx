42b2da8817ff326291d0932742d2bbb3e675e771
Amira Belkis <ia_feliachi@esi.dz>
2025-02-10 16:00:02 +0100

    add activities content

--- version-02/src/components/Activities/Activities.jsx
+++ version-02/src/components/Activities/Activities.jsx
@@ -1,19 +1,27 @@
 import Title from "../Title"
 import ActivitiesCard from "./ActivitiesCard"
+import activitiesData from "../../assets/activities/activities.json"
+
 
 const Activities = () => {
     return (
         <div id="activities">
             <Title> Activities </Title>
             <div className="d-grid gap-5 activities-grid-template">
-                <ActivitiesCard title="Dining Room" img="pressStart.png" description="Redesigned the menu for Dining Room restaurant
-Redesigned the menu for Dining Room restaurant"></ActivitiesCard>
-                <ActivitiesCard title="Dining Room" img="pressStart.png" description="Redesigned the menu for Dining Room restaurant
-Redesigned the menu for Dining Room restaurant"></ActivitiesCard>
-                <ActivitiesCard title="Dining Room" img="pressStart.png" description="Redesigned the menu for Dining Room restaurant
-Redesigned the menu for Dining Room restaurant"></ActivitiesCard>
+                {
+                    Object.entries(activitiesData).map((activities, index) => {
+                        const activity = activities[1];
+                        return (
+                            <ActivitiesCard
+                                key={index}
+                                img={activity.img}
+                                title={activity.title}
+                                description={activity.description}
+                            />
+                        )
+                    })
+                }
             </div>
-
         </div>
     )
 }
--- version-02/src/components/Activities/ActivitiesCard.jsx
+++ version-02/src/components/Activities/ActivitiesCard.jsx
@@ -4,7 +4,7 @@ const ActivitiesCard = ({ img, title, description }) => {
     return (
         <div className="card-custom m-2">
             <div className="card-body gap-1">
-                <img className="w-25 rounded-circle" src={`${activitiesAssetsLocation}/images/${img}`} alt={`Activity -${title}`} />
+                <img className="w-25 rounded-circle activities-img" src={`${activitiesAssetsLocation}/images/${img}`} alt={`Activity -${title}`} />
                 <h1 className="w-100"> {title} </h1>
                 <p className="text-center">{description}</p>
             </div>

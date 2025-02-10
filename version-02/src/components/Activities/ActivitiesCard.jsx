12fb30fbc1cb4c7687a54a385fef988a8590bc50
Amira Belkis <ia_feliachi@esi.dz>
2025-02-10 11:42:35 +0100

    update card style

--- version-02/src/components/Activities/ActivitiesCard.jsx
+++ version-02/src/components/Activities/ActivitiesCard.jsx
@@ -3,8 +3,8 @@ import { activitiesAssetsLocation } from "../../utils/constants"
 const ActivitiesCard = ({ img, title, description }) => {
     return (
         <div className="card-custom m-2">
-            <div className="card-body">
-                <img className="w-25" src={`${activitiesAssetsLocation}/images/${img}`} alt={`Activity -${title}`} />
+            <div className="card-body gap-1">
+                <img className="w-25 rounded-circle" src={`${activitiesAssetsLocation}/images/${img}`} alt={`Activity -${title}`} />
                 <h1 className="w-100"> {title} </h1>
                 <p className="text-center">{description}</p>
             </div>
